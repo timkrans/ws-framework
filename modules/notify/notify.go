e3a3369f6a1051fa72bf2bbf04c64fb1575ec61a
timkrans <tkransberger@yahoo.com>
2026-03-03 10:24:45 -0500

    adding a notify modules to the framework

---
+++ modules/notify/notify.go
@@ -0,0 +1,37 @@
+package notify
+
+import (
+    "encoding/json"
+    "github.com/timkrans/ws-framework/auth"
+    "github.com/timkrans/ws-framework/events"
+    "github.com/timkrans/ws-framework/transport"
+)
+
+var ModuleConfig Config
+
+type Config struct {
+    Persist bool
+}
+
+type Handler struct{}
+
+func (h Handler) Handle(c interface{}, evt events.Event) {
+    client := c.(*transport.Client)
+
+    switch client.Source.(type) {
+    case *auth.RemoteAuth:
+        if evt.User != client.UserID {
+            return
+        }
+    default:
+        evt.User = client.UserID
+    }
+
+    out, _ := json.Marshal(evt)
+    client.Room.Broadcast <- out
+}
+
+func Init(cfg Config) {
+    ModuleConfig = cfg
+    events.Register("notify.send", Handler{})
+}
